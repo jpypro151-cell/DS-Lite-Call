f41cf2f5923bf96cfba991b8eba74896c2450c1b
jpypro151-cell <jpypro151@gmail.com>
2026-08-28 17:25:03 -0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,25 @@
+#include <nds.h>
+#include <stdio.h>
+
+int main() {
+    consoleDemoInit();
+
+    iprintf("\n\n");
+    iprintf("  Made by jpypro151\n");
+    iprintf("  First Homebrew\n");
+
+    while(1) {
+        // Scan keyboard/touchpad inputs
+        scanKeys();
+        
+        // Read if the START button was pressed
+        int pressed = keysDown();
+        if (pressed & KEY_START) {
+            break; // Exit loop if START is pressed
+        }
+        swiWaitForVBlank();
+    }
+
+    return 0;
+}
 
